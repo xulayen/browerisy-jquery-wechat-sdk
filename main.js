df22eb2784449a26617fc8b5a8cea06c92c53d88
xulayen <xulayen@163.com>
2017-11-23 09:42:03 +0800

    update

--- main.js
+++ main.js
@@ -1,8 +1,9 @@
 var unique = require('uniq');
-
 var _w = require('jquery_wechat_sdk');
 
 var data = [1, 2, 2, 3, 4, 5, 5, 5, 6];
+
+var http = require('http-browserify');
 
 console.log(unique(data));
 
@@ -11,8 +12,22 @@ var wechatMgr=_w.WeChart({
     timestamp: 'your timestamp',
     nonceStr: 'your nonceStr',
     signature: 'your signature ',
-    access_token:'your access_token',
+    access_token:'your access_token   12323',
     debug:true
 });
+
+
+http.get('http://www.baidu.com/', function (res) {
+    var div = document.getElementById('result');
+    div.innerHTML += 'GET /beep<br>';
+ 
+    res.on('data', function (buf) {
+        div.innerHTML += buf;
+    });
+ 
+    res.on('end', function () {
+        div.innerHTML += '<br>__END__';
+    });
+})
 
 wechatMgr.InitWeChat();
